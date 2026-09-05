56498854977bcbd30ab1a5673e9fe4f827686adb
fallenleavesguy <9261562+fallenleavesguy@users.noreply.github.com>
2026-09-05 22:11:59 +0800

    update

--- lib/screens/contacts.dart
+++ lib/screens/contacts.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:rolodex/data/contact.dart';
 import 'package:rolodex/data/contact_group.dart';
 
 import '../main.dart';
@@ -31,22 +32,79 @@ class _ContactListView extends StatelessWidget {
         builder: (context, contactGroups, child) {
           final contactList = contactGroupsModel.findContactList(listId);
 
+          final contacts = contactList.alphabetizedContacts;
+
           return CustomScrollView(
             slivers: [
-              CupertinoSliverNavigationBar(
+              CupertinoSliverNavigationBar.search(
                 largeTitle: Text(contactList.title),
                 automaticallyImplyLeading: automaticallyImplyLeading,
-              ),
-              SliverFillRemaining(
-                child: Center(
-                  child: Text(
-                    '${contactList.contacts.length} contacts in ${contactList.label}',
-                  ),
+                searchField: const CupertinoSearchTextField(
+                  suffixIcon: Icon(CupertinoIcons.mic_fill),
+                  suffixMode: OverlayVisibilityMode.always,
                 ),
+              ),
+              SliverList.list(
+                children: [
+                  const SizedBox(height: 20),
+                  ...contacts.keys.map(
+                    (initial) => ContactListSection(
+                      lastInitial: initial,
+                      contacts: contacts[initial]!,
+                    ),
+                  ),
+                ],
               ),
             ],
           );
         },
+      ),
+    );
+  }
+}
+
+class ContactListSection extends StatelessWidget {
+  const ContactListSection({
+    super.key,
+    required this.lastInitial,
+    required this.contacts,
+  });
+
+  final String lastInitial;
+  final List<Contact> contacts;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsetsDirectional.fromSTEB(20, 0, 20, 0),
+      child: Column(
+        children: [
+          const SizedBox(height: 15),
+          Align(
+            alignment: AlignmentDirectional.bottomStart,
+            child: Text(
+              lastInitial,
+              style: const TextStyle(
+                color: CupertinoColors.systemGrey,
+                fontSize: 15,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+          CupertinoListSection(
+            backgroundColor: CupertinoColors.systemBackground,
+            dividerMargin: 0,
+            additionalDividerMargin: 0,
+            topMargin: 4,
+            children: [
+              for (final Contact contact in contacts)
+                CupertinoListTile(
+                  padding: const EdgeInsets.all(0),
+                  title: Text('${contact.firstName} ${contact.lastName}'),
+                ),
+            ],
+          ),
+        ],
       ),
     );
   }
